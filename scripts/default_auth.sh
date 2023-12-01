232094d0e4988245552730fa9cd4a383fcc37ca9
bal7hazar <bal7hazar@proton.me>
2023-12-01 12:05:04 +0100

    🐛 Fix auth script

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,17 +2,16 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export WORLD_ADDRESS="0x7d1f066a910bd86f532fa9ca66766722c20d47462fb99fb2fb0e1030262f9c5";
+export WORLD_ADDRESS="0x45913f69140d1fb00db13514f06cd0503838a75c076637086a25d385e732cbb";
+export HOST_ADDRESS="0x9e296cd237ed0ed2f9d65950c6c708c10c4620fdbd76b1a3c1e014a07391f3"
+export PLAY_ADDRESS="0x361fc51ac38dde141e478a0f2dde0f5a29797b0e1a3fae9ee3dce9fc681b8a8"
 
-# enable system -> component authorizations
-COMPONENTS=("Position" "Moves" )
+# enable system -> model authorizations
 
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component spawn --world $WORLD_ADDRESS
-done
-
-for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component move --world $WORLD_ADDRESS
+MODELS=("Game" "Player", "Tile")
+for model in ${MODELS[@]}; do
+    sozo auth writer $model $HOST_ADDRESS --world $WORLD_ADDRESS
+    sozo auth writer $model $PLAY_ADDRESS --world $WORLD_ADDRESS
 done
 
 echo "Default authorizations have been successfully set."
