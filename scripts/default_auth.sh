ba09868ede0e7ed30e13cfa02207ee202b78acb7
bal7hazar <bal7hazar@proton.me>
2024-02-24 10:27:37 +0100

    🚀 Deploy new world to slot

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-# export RPC_URL="http://localhost:5050";
-export RPC_URL="https://api.cartridge.gg/x/zconqueror/katana";
+export RPC_URL="http://localhost:5050";
+# export RPC_URL="https://api.cartridge.gg/x/zconqueror/katana";
 
 export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
 export HOST_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "zconqueror::systems::host::host" ).address')
@@ -19,11 +19,12 @@ echo "--------------------------------------------------------------------------
 # enable system -> model authorizations
 
 MODELS=("Game" "Player" "Tile")
+CONTRACTS=($HOST_ADDRESS $PLAY_ADDRESS)
 for model in ${MODELS[@]}; do
-    sozo auth writer $model $HOST_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
-    sleep 5
-    sozo auth writer $model $PLAY_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
-    sleep 5
+    for contract in ${CONTRACTS[@]}; do
+        sozo auth writer $model $contract --world $WORLD_ADDRESS --rpc-url $RPC_URL
+        sleep 1
+    done
 done
 
 echo "Default authorizations have been successfully set."
